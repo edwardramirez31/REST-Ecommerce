0f7bad4de1b629f9f781b1720396a76d8923a167
Edward Ramirez <edal_ramirez@hotmail.com>
2021-05-23 15:08:05 -0500

    Update, Read and Delete Functionality
    Django Rest Framework provides three generic views for handling common operations as update, read and delete in a database.
    For update, I used UpdateAPIView with its serializer class as ProductSerializer. It also needs a get_queryset method
    to tell Django the queryset where it performs the id (passed through the URL) filtering. This view also has the "patch"
    method for getting the record information and the "put" method for handling extra logic in this generic view.
    
    The other ones are practically the same, Delete and Retrieve API views need a serializer_class as attribute and the get_queryset
    method to filter that query by id. The "delete" method in ProductDeleteAPIView was overwritten because I didn't want to delete
    the record from the database, just put its state as False and save that. If the object exists it returns a success message, otherwise
    it returns 404

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -4,13 +4,19 @@ from apps.products.api.views.general_views import (CategoryProductListView,
                                                    IndicatorListView,
                                                    MeasureUnitListView)
 from apps.products.api.views.product_views import (ProductCreateAPIView,
-                                                   ProductListView)
+                                                   ProductDeleteAPIView,
+                                                   ProductListView,
+                                                   ProductRetrieveAPIView,
+                                                   ProductUpdateAPIView)
 
 app_name = "products"
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
     path('create/', ProductCreateAPIView.as_view(), name='create'),
+    path('delete/<int:pk>/', ProductDeleteAPIView.as_view(), name='delete'),
+    path('detail/<int:pk>/', ProductRetrieveAPIView.as_view(), name='detail'),
+    path('update/<int:pk>/', ProductUpdateAPIView.as_view(), name='update'),
     path('measure_units/', MeasureUnitListView.as_view(), name='measure_units'),
     path('category/', CategoryProductListView.as_view(), name='category'),
     path('indicator/', IndicatorListView.as_view(), name='indicator'),
